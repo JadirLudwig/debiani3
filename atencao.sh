bfafb15524c0251ab9d8de9778d8819308184c9a
JadirLudwig <jadir.ludwig@gmail.com>
2018-07-10 23:46:27 -0300

    Add files via upload

---
+++ atencao.sh
@@ -0,0 +1,121 @@
+# !/bin/bash
+clear
+echo
+sleep 0.5
+echo "       ###########"
+sleep 0.5
+echo "    ################."
+sleep 0.5
+echo "  #######        #####"
+sleep 0.5
+echo " ######            #####"
+sleep 0.5
+echo "#####       ####.     ###     #####       ############"
+sleep 0.5
+echo "####     ####    :    ###     #####       ############"
+sleep 0.5
+echo "###      ###    /     ###                         ####"
+sleep 0.5
+echo "###      ###          ###     #####               ####"
+sleep 0.5
+echo "###      ###         ###      #####       ############"
+sleep 0.5
+echo " ###      ###       ###       #####       ############"
+sleep 0.5
+echo "  ###       ###.#.###         #####               ####"
+sleep 0.5
+echo "    ###                       #####               ####"
+sleep 0.5
+echo "     ###                      #####       ############"
+sleep 0.5
+echo "       ###                    #####       ############"
+sleep 0.5
+echo "         ###"
+sleep 0.5
+echo "            ###    ATENÇÃO  ATENÇÃO  ATENÇÃO  ATENÇÃO "
+echo
+echo "                A MÁQUINA SERÁ REINICIADA EM INSTANTES,"
+echo " APÓS A MÁQUINA REINICIAR EXECUTE O SCRIPT EXECUTE2.SH"
+echo "                           PARA CONTINUAR A INSTALAÇÃO"
+echo
+sleep 2
+echo "REINICIANDO A MAQUINA"
+echo
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo -n "#"
+sleep 0.2
+echo "#"
+sleep 0.2
+
+reboot
