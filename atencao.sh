6cec3403cc80eecf839c12f2d3a148e6dff40b11
JadirLudwig <jadir.ludwig@gmail.com>
2018-07-11 00:00:04 -0300

    Add files via upload

--- atencao.sh
+++ atencao.sh
@@ -33,12 +33,21 @@ sleep 0.5
 echo "         ###"
 sleep 0.5
 echo "            ###    ATENÇÃO  ATENÇÃO  ATENÇÃO  ATENÇÃO "
+sleep 0.5
 echo
 echo "                A MÁQUINA SERÁ REINICIADA EM INSTANTES,"
+sleep 0.5
 echo " APÓS A MÁQUINA REINICIAR EXECUTE O SCRIPT EXECUTE2.SH"
-echo "                           PARA CONTINUAR A INSTALAÇÃO"
+sleep 0.5
+echo "        NA PASTA DEBIANI3 PARA CONTINUAR A INSTALAÇÃO!"
+sleep 0.5
 echo
-sleep 2
+echo "           Para abrir o terminal no i3 use mod + Enter"
+sleep 0.5
+echo "      A tecla mod será a tecla Super (Windonws) ou Alt"
+sleep 0.5
+echo
+sleep 10
 echo "REINICIANDO A MAQUINA"
 echo
 echo -n "#"
